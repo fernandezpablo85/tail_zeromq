99454394ab41ba01c7f97d30f7eee1a7eeb9f737
Pablo Fernandez <fernandezpablo85@gmail.com>
2013-05-11 21:12:32 -0300

    initial working version

---
+++ server.js
@@ -0,0 +1,43 @@
+var app = require('express')(),
+    server = require('http').createServer(app), 
+    io = require('socket.io').listen(server),
+    zmq = require('zmq');
+
+io.configure(function(){
+  io.set('log level', 1);
+})
+
+server.listen(8080);
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function (socket) {
+
+  var listener = zmq.socket('sub');
+  listener.connect('tcp://127.0.0.1:3000');
+  listener.subscribe("data")
+  listener.on("message", function(message) {
+    console.log("listened!");
+    socket.emit('zmq', message.toString("ascii"));
+  });
+
+});
+
+
+// emitter.
+
+sock = zmq.socket('pub');
+
+sock.bindSync('tcp://127.0.0.1:3000');
+
+setInterval(function() {
+  var data = {
+    "topic" : "news",
+    "title" : "River wins local championship.",
+    "body"  : "Loren ipsum, we're champions fuck everyone else."
+  }
+  console.log("seding shit")
+  sock.send("data " + JSON.stringify(data));
+}, 10);
